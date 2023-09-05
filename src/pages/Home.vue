7a248f704e507a9d2f10f4e802ebab5c48434e15
Maksym <citomax7196@gmail.com>
2023-09-05 19:55:11 +0200

    general code home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,107 @@
 <script setup>
+import { ref } from "vue";
+import { useRootStore } from "@/stores/root";
+import { storeToRefs } from "pinia";
+import AppLayout from "../components/AppLayout.vue";
+import CocktailThumb from "../components/CocktailThumb.vue";
+
+const rootStore = useRootStore();
+rootStore.getIngredients();
+
+const { ingredients, cocktails } = storeToRefs(rootStore);
+const ingredient = ref(null);
+
+function getCocktails() {
+  rootStore.getCocktails(ingredient.value);
+}
 </script>
 
-<template>Home</template>
+<template>
+  <AppLayout imgUrl="/src/assets/img/bg-1.jpg">
+    <div class="wrapper">
+      <div class="info" v-if="!ingredient || !cocktails">
+        <div class="title">Choose your drink</div>
+        <div class="line"></div>
+        <div class="select-wrapper">
+          <el-select
+            v-model="ingredient"
+            placeholder="Choose main ingredient"
+            size="large"
+            class="select"
+            @change="getCocktails"
+          >
+            <el-option
+              v-for="item in ingredients"
+              :key="item.strIngredient1"
+              :label="item.strIngredient1"
+              :value="item.strIngredient1"
+            />
+          </el-select>
+        </div>
+        <div class="text">
+          Try our delicious cocktail recipes for every occasion. Find delicious
+          cocktail recipes by ingredient through our cocktail generator.
+        </div>
+        <img class="img" src="/src/assets/img/cocktails.png" alt="Cocktails" />
+      </div>
+      <div v-else class="info">
+        <div class="title">COCKTAILS WITH {{ ingredient }}</div>
+        <div class="line"></div>
+        <div class="cocktails">
+          <CocktailThumb
+            v-for="cocktail in cocktails"
+            :key="cocktail.idDrink"
+            :cocktail="cocktail"
+          />
+        </div>
+      </div>
+    </div>
+  </AppLayout>
+</template>
 
 <style lang="sass" scoped>
+@import '../assets/styles/main'
+.wrapper
+  display: flex
+  justify-content: center
+  align-items: center
+.info
+  padding: 80px 0
+  text-align: center
+
+.select-wrapper
+  padding-top: 50px
+
+.select
+  width: 220px
+
+.text
+  max-width: 516px
+  margin: 0 auto
+  padding-top: 50px
+  line-height: 36px
+  letter-spacing: 0.1em
+  color: $textMuted
+
+.img
+  margin-top: 60px
+
+.cocktails
+  display: flex
+  justify-content: space-between
+  align-items: center
+  flex-wrap: wrap
+  max-height: 420px
+  overflow-y: scroll
+  margin-top: 60px
+
+.cocktails::-webkit-scrollbar
+  width: 0.5em
+
+.cocktails::-webkit-scrollbar-track
+  background-color: transparent
+
+.cocktails::-webkit-scrollbar-thumb
+  background-color: transparent
+  border: none
 </style>
